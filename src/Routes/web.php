7ad1f4ee9487eab0dc0a2a605ae7ce6cd44eea87
Ederson Fernandes <edersontec@outlook.com>
2025-02-12 14:59:33 -0300

    fix: criação de rota padrão para capturar todas as rotas não declaradas

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,11 +3,18 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
+use Slim\Exception\HttpNotFoundException;
 use App\Controllers\ValidacaoController;
 
 return function (App $app) {
 
-    $app->post( '/validar', [ValidacaoController::class, 'validarCartao'] );
+    $app->post('/validar', [ValidacaoController::class, 'validarCartao']);
 
     // Outras rotas
+
+    // Rota padrão para capturar todas as rotas não declaradas
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function (Request $request, Response $response) {
+        $response->getBody()->write(json_encode(['error' => 'Rota não encontrada']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
+    });
 };
